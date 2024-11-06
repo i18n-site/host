9fc82930ba3eda696ce10e0f15122c7724d08dd6
i18n <i18n.site@gmail.com>
2024-11-06 15:57:54 +0800

    ♨

--- acme/src/lib.rs
+++ acme/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-  left + right
-}
+use std::time::Duration;
 
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
-  }
-}
+use acme2_eab::{
+  gen_rsa_private_key, AccountBuilder, AuthorizationStatus, ChallengeStatus, Csr, DirectoryBuilder,
+  Error, OrderBuilder, OrderStatus,
+};
